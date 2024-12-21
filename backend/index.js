8bc5bc39a7375adb0fc8e247102ac9fd369c629f
Priyanshu Sahu <priyanshusahu085@gmail.com>
2024-12-21 15:42:06 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 config({ path: ".env" }); 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://employee-management-system-roan-pi.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
 }));

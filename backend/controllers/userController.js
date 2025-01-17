88a1344e7285d4236ce3d988949b61cf8c0b15ec
Priyanshu Sahu <priyanshusahu085@gmail.com>
2025-01-17 22:47:57 +0530

    updates added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const getUserData = async (req, res) => {
   try {
@@ -28,4 +29,34 @@ const deleteUserData = async (req, res) => {
   }
 };
 
-export { getUserData, deleteUserData };
+const updatePassword = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(userId);
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+      return res
+        .json({ success: false, message: "Wrong Old Password!" });
+    }
+    
+    const hashPassword = await bcrypt.hash(newPassword, 10);
+
+    await User.findByIdAndUpdate(
+      userId,
+      { password: hashPassword },
+      { new: true }
+    );
+
+    return res.json({
+      success: true,
+      message: "Password Updated Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export { getUserData, deleteUserData, updatePassword };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { getUserData, deleteUserData } from "../controllers/userController.js";
+import { getUserData, deleteUserData, updatePassword } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getUserData);
 router.delete("/delete/:userId", authMiddleware, deleteUserData);
+router.post("/update-password/:userId", authMiddleware, updatePassword);
 
 export default router;
